06e0dabbfee1e6b1e9987373cee9e34360f8e452
jessicabui0110 <100547377+jessicabui0110@users.noreply.github.com>
2023-10-15 15:46:51 +1100

    Update visualisation_ourdata.py

--- visualisation_ourdata.py
+++ visualisation_ourdata.py
@@ -26,13 +26,17 @@ class SegmentationThread(QThread):
     finishedSignal = pyqtSignal(str)
     progressSignal = pyqtSignal(int)
     resetProgressSignal = pyqtSignal()
+    updatePieChartSignal = pyqtSignal(object)
 
-    def __init__(self, patch_names):
+    def __init__(self, model_file_path, patch_names):
         super(SegmentationThread, self).__init__()
+        print("Patch Names:", patch_names)
+        print("Model File Path:", model_file_path)
+        self.model_file_path = model_file_path or "swin-t-pixel-classification-final-epoch-200.pth"
         self.patch_names = patch_names
         self.is_stopped = False
         self.is_paused = False
-
+        
     def run(self):
         try:
 
@@ -81,7 +85,11 @@ class SegmentationThread(QThread):
                 model = DoubleSwinTransformerSegmentationS2(s2_backbone, out_dim=data_config['num_classes'], device=device)
 
                     # load desired segmentation checkpoint (pick in GUI)
-                model.load_state_dict(torch.load("swin-t-pixel-classification-final-epoch-200.pth", map_location='cpu')) # replace path with desired checkpoint
+                
+                # model.load_state_dict(torch.load("swin-t-pixel-classification-final-epoch-200.pth", map_location='cpu')) # replace path with desired checkpoint
+                
+                # print(type(self.model_file_path), self.model_file_path)
+                model.load_state_dict(torch.load(self.model_file_path, map_location='cpu'))
                 model.to(device)
 
                     # array of patch names (feed in from input.csv file or pick in GUI)
@@ -92,6 +100,7 @@ class SegmentationThread(QThread):
 
                 all_output_arrays = []
                 for index, patch_name in enumerate(self.patch_names):
+                        print("Patch name: " + patch_name)
 
                         progress_percentage = int((index + 1) / len(self.patch_names) * 100)
                         self.progressSignal.emit(progress_percentage)
@@ -121,16 +130,16 @@ class SegmentationThread(QThread):
                         print(mpc_tensor.shape) # output is now [1, 13, 224, 224] as desired
                         
                         # control logic 
-                        for patch_name in self.patch_names:
-                            if self.is_stopped:
+                        
+                        if self.is_stopped:
                                 print("Stop flag detected. Select the patches to start the segmentation again")
                                 self.resetProgressSignal.emit()
                                 return
 
-                            if self.is_paused:
+                        if self.is_paused:
                                 print("Segmentation is paused")
                                 
-                            while self.is_paused:
+                        while self.is_paused:
                                 time.sleep(1)
 
 
@@ -179,8 +188,8 @@ class SegmentationThread(QThread):
                                     mpc_tensor = F.pad(mpc_tensor, (0, y_l, x_l, 0, 0, 0, 0, 0), "constant", 0) # prepend difference to x, append difference to y
                                 elif c: # check (1, 1) exists
                                     mpc_tensor = F.pad(mpc_tensor, (0, y_l, 0, x_l, 0, 0, 0, 0), "constant", 0) # append difference to x, append difference to y
-                            print(mpc_tensor)
-                            print(mpc_tensor.shape)
+                            # print(mpc_tensor)
+                            # print(mpc_tensor.shape)
                         patch_img = {"s2": mpc_tensor} # create dictionary using same format as DFC
 
                         # evaluate using model
@@ -210,6 +219,7 @@ class SegmentationThread(QThread):
                         os.makedirs(output_folder, exist_ok=True)
 
                         # Get the filename of the current TIF patch
+                        print("Patch name: " + patch_name)
                         tif_filename = patch_name
 
                         # Remove the file extension to use as data_info
@@ -263,7 +273,8 @@ class SegmentationThread(QThread):
                         print(f"GeoTIFF file '{output_tif_filename}' created.")
 
                         npy_output_folder = "npy_outputs"
-                        os.makedirs(npy_output_folder, exist_ok=True)  
+                        os.makedirs(npy_output_folder, exist_ok=True)
+                        self.updatePieChartSignal.emit(all_output_arrays)  
                         combined_npy_filename = os.path.join(npy_output_folder, "all_output_arrays.npy")
                         np.save(combined_npy_filename, all_output_arrays)
 
